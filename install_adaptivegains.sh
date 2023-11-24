70dcbf51b2b6d966288d211e2214580606883610
mypiaware <40304323+mypiaware@users.noreply.github.com>
2023-11-24 00:38:44 -0500

    Updated for PiAware 9.x

--- install_adaptivegains.sh
+++ install_adaptivegains.sh
@@ -4,36 +4,53 @@
 # https://github.com/mypiaware/flightaware_adaptive_gain_listing
 
 AGCMD="/usr/local/bin/adaptivegains"
-URL="https://github.com/mypiaware/flightaware_adaptive_gain_listing/raw/main/adaptivegains.sh"
-CHECKSUM="ffd66b18206d4465d7733e3c3a7ff2f2"  # Checksum of 'adaptivegains.sh' (verson 1.2).  [Needs to be all lowercase]
+URL="https://github.com/mypiaware/flightaware_adaptive_gain_listing/raw/main/install_adaptivegains.sh"
+CHECKSUM="5dc4de162b463ec06704d133a7d5087e"  # MD5 Checksum of 'adaptivegains.sh' (verson 2.0).  [Needs to be all lowercase]
+LATESTVERSION="2.0"
 
-if ! [ -f "$AGCMD" ]; then
+if [ -f "$AGCMD" ] && [[ $(md5sum "$AGCMD") =~ $CHECKSUM ]]; then
+   printf "The installed version of 'adaptivegains' is the latest version (version $LATESTVERSION).\n"
+   exit 0
+elif [ -f "$AGCMD" ] && [[ ! $(md5sum "$AGCMD") =~ $CHECKSUM ]]; then
+   while ! [[ $USERCHOICE =~ ^[YyNn]$ ]]; do printf "Download & update the 'adaptivegains' command? [y/n]: "; read USERCHOICE; done
+   ACTION="update"
+elif ! [ -f "$AGCMD" ]; then
    while ! [[ $USERCHOICE =~ ^[YyNn]$ ]]; do printf "Download & install the 'adaptivegains' command? [y/n]: "; read USERCHOICE; done
+   ACTION="install"
 else
-   while ! [[ $USERCHOICE =~ ^[YyNn]$ ]]; do printf "Download & reinstall/update the 'adaptivegains' command? [y/n]: "; read USERCHOICE; done
+   printf "Unknown error occurred!\n";
+   exit 3
 fi
 
-if [[ $USERCHOICE =~ [Yy] ]]; then
+if [ $ACTION = "update" ]; then
    sudo mv "$AGCMD" "${AGCMD}".old >/dev/null 2>&1
    printf "\n"
    sudo wget -q --show-progress "$URL" -O "$AGCMD"
    printf "\n"
-   if [ -f "$AGCMD" ] && [[ $(md5sum "$AGCMD") =~ $CHECKSUM ]] ; then
-      if ! [ -f "$AGCMD".old ]; then ACTION="installed"
-      elif diff "$AGCMD".old "$AGCMD" >/dev/null 2>&1; then ACTION="reinstalled"
-      else ACTION="updated"
-      fi
+   if [ -f "$AGCMD" ] && [[ $(md5sum "$AGCMD") =~ $CHECKSUM ]]; then
       sudo chmod +x "$AGCMD"
       sudo rm -f "${AGCMD}".old
-      printf "\033[1;32mSUCCESS: The 'adaptivegains' command was $ACTION!\033[0m\n\n"
+      printf "\033[1;32mSUCCESS: The 'adaptivegains' command was updated!\033[0m\n\n"
       exit 0
    else
-      if [ -f "${AGCMD}".old ]; then ACTION="update"; else ACTION="install"; fi
-      sudo rm -f "$AGCMD"
+      printf "\033[1;31mERROR: Failure trying to update the 'adaptivegains' command!\033[0m\n\n"
       sudo mv "${AGCMD}".old "$AGCMD" >/dev/null 2>&1
-      printf "\033[1;31mERROR: Failure trying to $ACTION the 'adaptivegains' command!\033[0m\n\n"
       exit 1
    fi
-else
-   exit 0
 fi
+
+if [ $ACTION = "install" ]; then
+   printf "\n"
+   sudo wget -q --show-progress "$URL" -O "$AGCMD"
+   printf "\n"
+   if [ -f "$AGCMD" ] && [[ $(md5sum "$AGCMD") =~ $CHECKSUM ]] ; then
+      sudo chmod +x "$AGCMD"
+      printf "\033[1;32mSUCCESS: The 'adaptivegains' command was installed!\033[0m\n\n"
+      exit 0
+   else
+      printf "\033[1;31mERROR: Failure trying to install the 'adaptivegains' command!\033[0m\n\n"
+      exit 2
+   fi
+fi
+
+exit 4  # Script should never reach this point.
